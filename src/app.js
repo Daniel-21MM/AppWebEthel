7a107c0877ac5b547b4b2bc300b10beac2fa82e0
Daniel-21MM <danielsanz8001@gmail.com>
2023-12-30 13:27:34 -0600

    Agregamos la vista de login

--- src/app.js
+++ src/app.js
@@ -1,23 +1,33 @@
-import express from 'express'
-import { pool } from './db.js'
-import {PORT} from './config.js'
+import express from 'express';
+import { pool } from './db.js';
+import { PORT } from './config.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+import path from 'path';
 
-app.get('/', async (req, res) => {
-  const [rows] = await pool.query('SELECT * FROM test')
-  res.json(rows)
-})
+const app = express();
+
+// Configuración para servir archivos estáticos desde la carpeta "views"
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.get('/', (req, res) => {
+  const indexPath = path.join(__dirname, 'views', 'index.html');
+  res.sendFile(indexPath);
+});
 
 app.get('/ping', async (req, res) => {
   const [result] = await pool.query(`SELECT "hello world" as RESULT`);
-  res.json(result[0])
-})
+  res.json(result[0]);
+});
 
-app.get('/create', async (req, res) => {
-  const result = await pool.query('INSERT INTO test(name) VALUES ("John")')
-  res.json(result)
-})
+// app.get('/create', async (req, res) => {
+//   const result = await pool.query('INSERT INTO test(name) VALUES ("John")');
+//   res.json(result);
+// });
 
-app.listen(PORT)
-console.log('Server on port', PORT)
+app.listen(PORT, () => {
+  console.log('Server on port', PORT);
+});
