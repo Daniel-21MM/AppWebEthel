03c243f54ae5023b3a3f5a62f9f446ee83d688a3
Daniel-21MM <danielsanz8001@gmail.com>
2023-12-30 12:39:21 -0600

    Debe jalar

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import {PORT} from './config.js'
 const app = express()
 
 app.get('/', async (req, res) => {
-  const [rows] = await pool.query('SELECT * FROM users')
+  const [rows] = await pool.query('SELECT * FROM test')
   res.json(rows)
 })
 
@@ -15,7 +15,7 @@ app.get('/ping', async (req, res) => {
 })
 
 app.get('/create', async (req, res) => {
-  const result = await pool.query('INSERT INTO users(name) VALUES ("John")')
+  const result = await pool.query('INSERT INTO test(name) VALUES ("John")')
   res.json(result)
 })
 
