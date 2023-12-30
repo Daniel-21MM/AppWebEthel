f729d88258672dcc5fb979ad4a57ed10684df9f7
Daniel-21MM <danielsanz8001@gmail.com>
2023-12-30 14:55:21 -0600

    depurando

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 3000
 
-export const DB_HOST = process.env.DB_HOST || 'b3pktkex5xv1rqhte0ht-mysql.services.clever-cloud.com'
+export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_USER = process.env.DB_USER || 'uhlnf3rrmexpvqmb'
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'b0qCqrdf2ouRbL7EDzA9'
 export const DB_NAME = process.env.DB_NAME || 'b3pktkex5xv1rqhte0ht'
