2a2cd7571e9cd55ae17c36c91978fbf0c6f64924
Daniel-21MM <danielsanz8001@gmail.com>
2023-12-30 20:02:47 -0600

    haber si jala

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.static(path.join(__dirname, 'public')));
